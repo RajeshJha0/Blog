a38b0e75c8533bf6fc45872c975122c8a2ee3177
RajeshJha0 <raj.jha0503@gmail.com>
2023-09-08 20:30:43 +0530

    new changes

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-console.log('This is Index')
-console.log('This is Index')
-console.log('This is Index')
-console.log('This is Index')
+const mongoose = require("mongoose")
+
+mongoose.connect("mongodb+srv://dbuser:dbuserpassword@blogcluster.sxsww1w.mongodb.net/");
+
+const express = require("express");
+const app = express();
+
+app.get('/',function(req,res){
+    res.send('Hii!');
+});
+
+app.listen(3000,function(){
+    console.log("Server is running");
+});
+
